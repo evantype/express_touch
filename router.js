b71d45c3de340a6bf814b2333289916a9d7b9d0a
Evgeniy Antipov <evantype@gmail.com>
2022-01-31 00:13:17 +0300

    begin routing

--- router.js
+++ router.js
@@ -1,8 +1,19 @@
 import Router from 'express'
+import Post from "./Post.js";
 
 const router = new Router();
 
-router.post('/posts');
+router.post('/posts', async (req, res) => {
+    try {
+        const {author, title, content, picture} = req.body;
+
+        const post = await Post.create({author, title, content, picture});
+
+        res.json(post)
+    } catch (e) {
+        res.status(500).json(e);
+    }
+});
 router.get('/posts');
 router.get('/posts/:id');
 router.put('/posts/');
