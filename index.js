7ba9179a6df40252c6f32f698fdc9c8c454fbc53
Evgeniy Antipov <evantype@gmail.com>
2022-01-30 22:37:18 +0300

    add mongo

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from 'express';
+import mongoose from 'mongoose'
 
 const PORT = 5000;
+const DB_URL = `mongodb+srv://epytnave:243598mo@cluster0.lnmpm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 
 const app = express();
 
@@ -10,4 +12,13 @@ app.post('/', (req, res) => {
     res.status(200).json('Server working')
 })
 
-app.listen(PORT, () => console.log('Server started on port ' + PORT));
+async function startApp() {
+    try {
+        await mongoose.connect(DB_URL);
+        app.listen(PORT, () => console.log('Server started on port ' + PORT));
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+startApp();
